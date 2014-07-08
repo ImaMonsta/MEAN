22bfd1ef022189a7ddcdd5dfe5103e3ad6b2830c
German <german@imamonsta.com>
2014-07-07 21:54:30 -0500

    server look up error

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ Message.findOne().exec(function(err,messageDoc){
 
 
 app.get('/partials/:partialPath', function(req, res){
-	res.render('partials/' + req.params.partialPath);
+	res.render('/partials/' + req.params.partialPath);
 });
 
 app.get('*', function(req, res){
