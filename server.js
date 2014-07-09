26f261ef4647d7968c45e3e48abf1a18e6c0c534
German <german@imamonsta.com>
2014-07-08 22:57:50 -0500

    Ready for login part 2 missing :dancer:

--- server.js
+++ server.js
@@ -1,60 +1,18 @@
 var express = require('express'),
-	stylus = require('stylus'),
-	logger = require('morgan'),
-	bodyParser = require('body-parser'),
 	mongoose = require('mongoose');
-//console.log('Hola');
 
-var MONGOHQ_URL='mongodb://imadbuser:Soporte01@ds043368.mongolab.com:43368/imadb'
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var app = express();
 
-function compile(str, path){
-	return stylus(str).set('filename', path);
-}
+var config = require('./server/config/config')[env];
 
-console.log(__dirname);
-app.set('views', __dirname + '/server/views');
-app.set('view engine', 'jade');
-app.use(logger('dev'));
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-app.use(bodyParser.json())
-app.use(stylus.middleware(
-	{
-		src: __dirname + '/public',
-		compile: compile		
-	}
-));
-app.use(express.static(__dirname + '/public'));
+require('./server/config/express')(app, config);
 
-/////////////////MONGOOSE
-mongoose.connect(MONGOHQ_URL);
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'conection error ...'));
-db.once('open', function callback(){
-	console.log('karuru db open');
-});
+require('./server/config/mongoose')(config);
 
-var messageSchema = mongoose.Schema({message: String});
-var Message = mongoose.model('messages', messageSchema);
-var mongoMessage;
-Message.findOne().exec(function(err,messageDoc){
-	mongoMessage = messageDoc.message;
-});
+require('./server/config/routes')(app);
 
-
-app.get('/partials/:partialPath', function(req, res){
-	res.render('Partials/' + req.params.partialPath);
-});
-
-app.get('*', function(req, res){
-	res.render('index', {mongoMessage: mongoMessage});
-});
-
-var port = process.env.PORT || 3030;
-app.listen(port);
-console.log('Listen on port ' + port + '...');
+app.listen(config.port);
+console.log('Listen on port ' + config.port + '...');
